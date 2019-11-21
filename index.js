ec88453f8738069bc666063fbaf85ad118b77d38
LuKks <lucasbarrena@hotmail.com>
2019-11-20 22:24:00 -0300

    standardizing style

--- index.js
+++ index.js
@@ -6,50 +6,50 @@
 
 'use strict';
 
-function extendSocket(base) {
-  if(typeof base.Socket.prototype.terminated !== 'undefined') return true;
+if (!extendSocket(require('net'))) {
+  extendHttp(require('http'));
+  extendHttp(require('https'), true);
+}
+
+function extendSocket (base) {
+  if (typeof base.Socket.prototype.terminated !== 'undefined') return true;
 
   base.Socket.prototype.requests = 0;
   base.Socket.prototype.terminated = false;
-  base.Socket.prototype.terminate = function() {
+  base.Socket.prototype.terminate = function () {
     this.terminated = true;
     return this.requests ? this.emit('terminate') : this.end(this.destroy);
   }
 
   let create = base.createServer;
-  base.createServer = function(options, listener) {
+  base.createServer = function (options, listener) {
     return create.call(base, options, listener).prependListener('connection', connection);
   }
 
   base.Server.prototype.terminated = false;
 
   let close = base.Server.prototype.close;
-  base.Server.prototype.close = function(callback) {
+  base.Server.prototype.close = function (callback) {
     close.call(this, callback);
     this.terminated = true;
     return this.emit('terminate');
   }
 }
 
-function connection(socket) {
+function connection (socket) {
   let term = () => socket.terminate();
   this.once('terminate', term);
   socket.once('close', () => this.removeListener('terminate', term));
 }
 
-function extendHttp(base, secure) {
+function extendHttp (base, secure) {
   let create = base.createServer;
-  base.createServer = function(options, listener) {
+  base.createServer = function (options, listener) {
     return create.call(base, options, listener)
     .prependListener(secure ? 'secureConnection' : 'connection', connection)
-    .prependListener('request', function(req, res) {
+    .prependListener('request', function (req, res) {
       req.connection.requests++;
       res.on('finish', () => !--req.connection.requests && this.terminated && req.connection.end(req.connection.destroy));
     });
   }
-}
-
-if(!extendSocket(require('net'))) {
-  extendHttp(require('http'));
-  extendHttp(require('https'), true);
 }
